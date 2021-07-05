00c681b52606f90ad15913fe59525804c64a00e8
kyooz <miftahfaiz84@gmail.com>
2021-07-05 07:53:55 +0700

    edit dbconnect

--- utils/db.js
+++ utils/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://127.0.0.1:27017/jogjavacation'
+    // url: 'mongodb://127.0.0.1:27017/jogjavacation'
+    url: 'mongodb+srv://jogjavacation:<jogjavacation>@jogjavacation.bente.mongodb.net/jogjavacation?retryWrites=true&w=majority'
 }
